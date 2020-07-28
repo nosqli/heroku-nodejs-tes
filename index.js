a2d8b0c65ed091a609ec4bce9e84d8fa5eac75ca
Pawel Malita <pawel.malita@netguru.com>
2020-07-28 17:25:40 +0200

    Some fixes

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-require('sqreen');
-const express = require('express')
+const express = require('express');
+const util = require('util');
 const app = express()
 const port = 3000
-app.get('/', (req, res) => res.send(req.params.toString()))
+app.get('/', (req, res) => {
+    res.send('<html><head></head><body><pre>'+util.inspect(req)+'</pre></body></html>');
+});
+app.listen(port);
